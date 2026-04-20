6f977a049e306cef6b5444276d4d01bf0e70f0c7
Seong-Hyeok You <motorsline14@gmail.com>
2026-04-20 10:40:34 +0900

    cal

---
+++ main.py
@@ -0,0 +1,15 @@
+print("A:")
+A = int(input())
+
+print("B:")
+B = int(input())
+
+print("op:")
+op = input()
+
+if op == "+":
+    print(A + B)
+elif op == "-":
+    print(A - B)
+elif op == "*":
+    print(A * B)
